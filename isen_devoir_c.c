2988dff8e69d1a91ca12138d53633e9c072ec872
Sacha BOUTON <sbouton@unis.fr>
2024-11-28 13:59:59 +0100

    ajout createWord

--- isen_devoir_c.c
+++ isen_devoir_c.c
@@ -5,7 +5,21 @@
 #define PROJECT_NAME "isen-devoir-c"
 #define READ_LIMIT 200
 
-struct Word;
+struct Word {
+    char* word;
+    char* line_numbers;
+    int count; // occurence de mots
+    struct Word *next;
+};
+
+struct Word* createWord(const char* word, int line_number){
+    struct Word* new_word = (struct Word*)malloc(sizeof(struct Word)); //rajouter erreur
+    new_word -> word = strdup(word);
+    new_word -> line_numbers = line_number;
+    new_word -> count = 1;
+    new_word -> next = NULL;
+    return new_word;
+}
 
 int readFile(char* fileName){
     FILE *file;
