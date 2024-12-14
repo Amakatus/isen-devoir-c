2dbbb0a639c2dc5ac5d0c35becdd6921a0df699c
Amakatus <sachabouton@gmail.com>
2024-12-15 00:13:40 +0100

    refactor wildcard search, but still have problems

--- src/filters.c
+++ src/filters.c
@@ -2,10 +2,13 @@
 #include "string.h"
 #include <strings.h>
 #include "filters.h"
-#include "fnmatch.h"
+#include <stdlib.h>
 
 int check_if_wildcard(void* type){
     char* searchWord = (char *) type;
+    if (searchWord == NULL || strlen(searchWord) == 0) {
+        return -1;
+    }
     if(searchWord[0] == '*'){
         return 0;
     } if (searchWord[strlen(searchWord) - 1] == '*'){
@@ -32,19 +35,19 @@ int case_insensitive_match(void* data, void* type){
 int wildcard_match(void *data, void *type){
     struct Word* word = (struct Word*) data;
     char* searchWord = (char *) type;
-
-    if(check_if_wildcard(searchWord) == 0){
-        searchWord++;
-        return strstr(word->word, searchWord) != NULL;
-    } else if (check_if_wildcard(searchWord) == 1){
-        searchWord[strlen(searchWord) - 1] = '\0';
-        return strstr(word->word,searchWord) != NULL;
-    } else if (check_if_wildcard(searchWord) == 2){
-        searchWord++;
-        searchWord[strlen(searchWord) - 1] = '\0';
-        return strstr(word->word,searchWord) != NULL;
+    char* newSearchWord = strdup(searchWord);
+    switch (check_if_wildcard(searchWord)){
+        case 0:
+            newSearchWord++;
+            return strstr(word->word, newSearchWord) != NULL;
+        case 1:
+            newSearchWord[strlen(newSearchWord) - 1] = '\0';
+            return strstr(word->word, newSearchWord) != NULL;
+        case 2:
+            newSearchWord++;
+            newSearchWord[strlen(newSearchWord) - 1] = '\0';
+            return strstr(word->word, newSearchWord) != NULL;
     }
-
     return -1;
 }
 
